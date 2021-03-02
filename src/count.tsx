15026eaa909fb5486e0bbd2601d26e658f5df79a
Lijo Vijayan <36627449+lijovijayan@users.noreply.github.com>
2021-03-02 11:03:39 +0530

    Update count.tsx

--- src/count.tsx
+++ src/count.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
-import './index.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function Count(){
-    let [count,setCount]= useState(0)
+function Count() {
+  let [count, setCount] = useState(0);
 
-    let incCount=()=>{
-       setCount(count+1);
-        
+  let incCount = () => {
+    setCount(count + 1);
+  };
+
+  let decCount = () => {
+    setCount(count - 1);
+  };
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-    
-  let decCount=()=>{
-          setCount(count-1);
-          
-    }
-  
-    
-   return(
-        <div className="Index">
-        <h1 className="Counter">{count} </h1>
-        <button className="Inc-button" onClick={incCount}>+</button>
-        <button className="Dec-button" onClick={decCount}>-</button>
+    return color;
+  }
 
-        </div>
-        )
-     
+  return (
+    <div className="Index">
+      <h1 style={{ color: getRandomColor() }} className="Counter">
+        {count}{" "}
+      </h1>
+      <button className="Inc-button" onClick={incCount}>
+        +
+      </button>
+      <button className="Dec-button" onClick={decCount}>
+        -
+      </button>
+    </div>
+  );
 }
 
-export {Count} 
-
-
-    
+export { Count };
